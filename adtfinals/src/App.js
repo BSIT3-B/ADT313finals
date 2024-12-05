ce2af5f9230eb017aaa8311873b0c2d0ced0125c
John Darell Palma <147915648+BSIT3-B@users.noreply.github.com>
2024-12-06 01:03:47 +0800

    Added changes

---
+++ adtfinals/src/App.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import Login from './pages/Public/Login/Login';
+import Main from './pages/Main/Main';
+import Register from './pages/Public/Login/Register'; // Import Register component
+import Movie from './pages/Main/Movie/Movie';
+import Lists from './pages/Main/Movie/Lists/Lists';
+import Form from './pages/Main/Movie/Form/Form';
+
+const router = createBrowserRouter([
+  {
+    path: '/',               // The root path points to the login page
+    element: <Login />,
+  },
+  {
+    path: '/Register',        // Add the Register page route
+    element: <Register />,
+  },
+  {
+    path: '/main',
+    element: <Main />,
+    children: [
+     
+      {
+        path: '/main/movies',
+        element: <Movie />,
+        children: [
+          {
+            path: '/main/movies',
+            element: <Lists />,
+          },
+          {
+            path: '/main/movies/form/:movieId?',
+            element: <Form />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div className='App'>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
